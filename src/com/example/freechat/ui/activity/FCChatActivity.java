c75bccfbe753798166282a989e0776c6796ae66f
XDcedar <402289412@qq.com>
2015-05-06 18:31:49 +0800

    Add audio function
    new png, xml. Audio player and recorder, chatActivity can record and
    play audio now.

--- src/com/example/freechat/ui/activity/FCChatActivity.java
+++ src/com/example/freechat/ui/activity/FCChatActivity.java
@@ -3,29 +3,43 @@ package com.example.freechat.ui.activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.RemoteException;
+import android.text.style.LineHeightSpan.WithDensity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.FileHandler;
 
 import com.example.freechat.FCPushService;
 import com.example.freechat.R;
+import com.example.freechat.R.string;
 import com.example.freechat.aidl.AIDLChatActivity;
 import com.example.freechat.aidl.AIDLPushService;
 import com.example.freechat.audio.FCAudioPlayer;
+import com.example.freechat.audio.FCAudioRecorder;
 import com.example.freechat.storage.DatabaseHandler;
+import com.example.freechat.storage.FCFileHelper;
 import com.example.freechat.ui.FCActionBarActivity;
 import com.example.freechat.ui.FCMessage;
 import com.example.freechat.ui.FCMessageAdapter;
@@ -40,15 +54,25 @@ public class FCChatActivity extends FCActionBarActivity {
     private Button m_sendAudioButton;
     private EditText m_sendMessgeText;
     
+    private Button m_RecorderButton;
+    private View m_RecorderPopupView;
+    private ImageView m_RecordVolumeView;
+    private FCAudioPlayer m_player;
+    private FCAudioRecorder m_FCAudioRecorder;
+    private FCFileHelper m_FileHelper;
+    private String filepath;
+    private Handler m_RecorderHandler = new Handler();
+	private static final int POLL_INTERVAL = 100;
+	
     private FCMessageAdapter m_messageAdapter;
     private List<FCMessage> m_messageList;    
 
     private String m_userid;
     
     private DatabaseHandler m_dbhandler;
-    
-    private FCAudioPlayer m_player;
-    
+
+    private boolean inRecorderMode = false;
+
     private AIDLChatActivity.Stub mCallback = new AIDLChatActivity.Stub() {
 
 		@Override
@@ -91,9 +115,9 @@ public class FCChatActivity extends FCActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         initUI();
+        initAudio();
         initChatListFromDB();
         bindMyPushService();
-        m_player = new FCAudioPlayer(this);
     }
     
     @Override
@@ -106,11 +130,11 @@ public class FCChatActivity extends FCActionBarActivity {
     		updateMessageList(msg);
     	}
     	
-    	if(requestCode == FCChatActivity.AUD_REQUEST && resultCode == RESULT_OK) {
-    		String content = data.getStringExtra("content");
-    		FCMessage msg = new FCMessage(content, FCMessage.SEND_MESSAGE, FCMessage.TYPE_AUD);
-    		updateMessageList(msg);
-    	}
+//    	if(requestCode == FCChatActivity.AUD_REQUEST && resultCode == RESULT_OK) {
+//    		String content = data.getStringExtra("content");
+//    		FCMessage msg = new FCMessage(content, FCMessage.SEND_MESSAGE, FCMessage.TYPE_AUD);
+//    		updateMessageList(msg);
+//    	}
     	
     }
     
@@ -124,7 +148,11 @@ public class FCChatActivity extends FCActionBarActivity {
         m_sendTxtButton = (Button) findViewById(R.id.bt_send_txt);
         m_sendAudioButton = (Button) findViewById(R.id.bt_send_audio);
         m_sendMessgeText = (EditText) findViewById(R.id.et_chatinfo);
-        
+
+        m_RecorderButton = (Button) findViewById(R.id.recorder_button);
+        m_RecorderPopupView = (View) findViewById(R.id.voice_record_popup);
+        m_RecordVolumeView = (ImageView) findViewById(R.id.voice_record_volume);
+
         m_sendPictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -137,11 +165,69 @@ public class FCChatActivity extends FCActionBarActivity {
 			
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(FCChatActivity.this, FCAudioActivity.class);
-            	startActivityForResult(intent, AUD_REQUEST);
+				if (!inRecorderMode) {
+					InputMethodManager imm = (InputMethodManager)
+							getSystemService(FCChatActivity.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(m_sendAudioButton.getWindowToken(), 0);
+
+					m_sendTxtButton.setVisibility(View.GONE);
+					m_sendMessgeText.setVisibility(View.GONE);
+					m_RecorderButton.setVisibility(View.VISIBLE);
+					m_sendAudioButton.setText(R.string.text);
+					inRecorderMode = true;
+				} else {
+					m_RecorderButton.setVisibility(View.GONE);
+					m_sendTxtButton.setVisibility(View.VISIBLE);
+					m_sendMessgeText.setVisibility(View.VISIBLE);
+					m_sendAudioButton.setText(R.string.audio);
+					inRecorderMode = false;
+				}
 			}
 		});
         
+m_RecorderButton.setOnTouchListener(new View.OnTouchListener() {
+        	@Override
+        	public boolean onTouch(View view, MotionEvent event) {
+        		switch(event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					m_RecorderButton.setBackgroundColor(getResources().getColor(R.color.Steelblue));
+					m_RecorderButton.setText(R.string.release_to_send);
+					m_RecorderButton.setTextColor(getResources().getColor(R.color.Clearwhite));
+					//show popup
+					m_RecorderPopupView.setVisibility(View.VISIBLE);
+
+					Log.e("FCChatActivity onTouch()", "set popup visible");
+					//start recording
+					filepath = m_FileHelper.generateFileName();
+					m_FCAudioRecorder.startRecord(filepath);
+					Log.e("FCChatActivity onTouch()", "start recording, path:" + filepath);
+					m_RecorderHandler.postDelayed(m_PollTask, POLL_INTERVAL);
+					break;
+
+				case MotionEvent.ACTION_UP:
+					m_RecorderButton.setBackgroundColor(getResources().getColor(R.color.Skyblue));
+					m_RecorderButton.setText(R.string.hold_to_talk);
+					m_RecorderButton.setTextColor(getResources().getColor(R.color.Clearwhite));
+					//hide popup
+					m_RecorderPopupView.setVisibility(View.GONE);
+					//stop recording
+					m_RecorderHandler.removeCallbacks(m_PollTask);
+					m_FCAudioRecorder.stopRecord();
+					m_RecordVolumeView.setImageResource(R.drawable.amp1);
+					//make message
+					FCMessage msg = new FCMessage(filepath, FCMessage.SEND_MESSAGE, FCMessage.TYPE_AUD);
+		    		updateMessageList(msg);
+					break;
+
+				case MotionEvent.ACTION_MOVE:
+					// TODO cancel sending
+				default:
+					break;
+				}
+				return false;
+			}
+		});
+
         m_chatListView.setOnItemClickListener(new OnItemClickListener() {
         	
         	@Override
@@ -221,5 +307,45 @@ public class FCChatActivity extends FCActionBarActivity {
 		Intent intent = new Intent(FCChatActivity.this, FCPushService.class);
 		bindService(intent, mConnection, BIND_AUTO_CREATE);
 		startService(intent);
+	}
+	
+	private Runnable m_PollTask = new Runnable(){
+		public void run() {
+			double amp = m_FCAudioRecorder.getAmplitude();
+			updateDisplay(amp);
+			m_RecorderHandler.postDelayed(m_PollTask, POLL_INTERVAL);
+		}
+	};
+	
+	private void initAudio() {
+        m_player = new FCAudioPlayer(this);
+        m_FCAudioRecorder = new FCAudioRecorder();
+        m_FileHelper = new FCFileHelper(this);
+	}
+	
+	private void updateDisplay(double signalEMA) {
+		switch ((int) signalEMA) {
+		case 0: case 1:
+			m_RecordVolumeView.setImageResource(R.drawable.amp1);
+			break;
+		case 2: case 3:
+			m_RecordVolumeView.setImageResource(R.drawable.amp2);
+			break;
+		case 4: case 5:
+			m_RecordVolumeView.setImageResource(R.drawable.amp3);
+			break;
+		case 6: case 7:
+			m_RecordVolumeView.setImageResource(R.drawable.amp4);
+			break;
+		case 8: case 9:
+			m_RecordVolumeView.setImageResource(R.drawable.amp5);
+			break;
+		case 10: case 11:
+			m_RecordVolumeView.setImageResource(R.drawable.amp6);
+			break;
+		default:
+			m_RecordVolumeView.setImageResource(R.drawable.amp7);
+			break;
+		}
 	}
 }
